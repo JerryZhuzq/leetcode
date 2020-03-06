83b6fda94ee8df060b1b3537a8e3a40db1ac854e
Jerry <JerryZhu_@outlook.com>
2020-03-05 19:18:02 -0500

    25 125 126

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-a = [1,2,3,4]
-print(a[:2])
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+dummy = ListNode(0)
+head = ListNode(2)
+
+pre = head
+pre = dummy
+
+print(pre.val)
